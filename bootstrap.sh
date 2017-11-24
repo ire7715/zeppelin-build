b220df1f7d83edb5fc425d275fae2cafae0c5d46
ire7715 <ire7715@hotmail.com>
2017-11-24 14:16:06 +0800

    update for hadoop 2.7.3

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,9 @@ cd /usr/local/zeppelin/incubator-zeppelin
 sed -i 's/bower install --silent/bower install --silent --allow-root/' /usr/local/zeppelin/incubator-zeppelin/zeppelin-web/package.json
 sed -i 's/local\[\*\]/yarn-client/' /usr/local/zeppelin/incubator-zeppelin/spark/src/main/resources/interpreter-setting.json
 
-mvn clean package -Pspark-2.2 -Dspark.version=2.2.0 -Dhadoop.version=2.7.1 -Phadoop-2.7 -Pyarn -DskipTests
+mvn clean package -Pspark-2.2 -Dspark.version=2.2.0 -Dhadoop.version=2.7.3 -Phadoop-2.7 -Pyarn -DskipTests
 
 cd /usr/local/zeppelin/
-tar -czf zeppelin-h2.7.1-s2.2.0.tar incubator-zeppelin
+tar -czf zeppelin-h2.7.3-s2.2.0.tar incubator-zeppelin
 
 /bin/bash
